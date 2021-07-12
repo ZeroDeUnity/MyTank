3f2e496f96b3b0b2abb77d9b85e0462f07eb7866
zero <1506065358@qq.com>
2021-07-12 17:43:07 +0800

    敌人生成,未完成

--- Assets/Scripts/EnemyMananger.cs
+++ Assets/Scripts/EnemyMananger.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -26,13 +27,16 @@ public class EnemyMananger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        EnemyCount = 200;
+        //获取地图敌人配置数据DT
+        DataTable MapDataDT = MapDataMananger.instance.MapEnemyConfigDataDT;
+        //获取敌人数量
+        EnemyCount = int.Parse(MapDataDT.Rows[0]["EnemyCount"].ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
+        //更新敌人数量文本控件数值
         enemyCountText.text = EnemyCount.ToString();
     }
 }
